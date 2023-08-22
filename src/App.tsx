6254e4c5f41473895f6ee0dd38c6d0ab52b201f8
Anderson Santana <ander0308@yahoo.com.br>
2023-08-22 20:10:35 -0300

    feat: add context and hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import "./Styles.css"
+import Header from "./Components/Header";
+import Sidenav from "./Components/Sidenav";
+import { DataContextProvider } from "./Context/DataContext";
+import Resumo from "./Pages/Resumo";
+import "./Styles.css";
 
 function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <DataContextProvider>
+      <div>
+        <Sidenav />
+        <main>
+          <Header />
+          <Resumo />
+        </main>
+      </div>
+    </DataContextProvider>
+  );
 }
 
-export default App
+export default App;
