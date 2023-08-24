82f60a1951c112a0d799146b4efdbc6615b83861
Anderson Santana <ander0308@yahoo.com.br>
2023-08-24 00:13:34 -0300

    feat: mesbutton and meses

---
+++ src/Components/MesButton/index.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { useData } from "../../Context/DataContext";
+
+const style: React.CSSProperties = {
+  padding: "var(--gap-default)",
+  backgroundColor: "var(--color-3)",
+  border: "none",
+  borderRadius: "var(--gap-default)",
+  color: "var(--color-2)",
+  fontWeight: "600",
+  textTransform: "capitalize",
+};
+
+function nomeMes(n: number) {
+  const date = new Date();
+  date.setMonth(date.getMonth() + n);
+  const nome = new Intl.DateTimeFormat("pt-BR", {
+    month: "long",
+  }).format(date);
+
+  return nome;
+}
+
+const formatDate = (date: Date) => {
+  const dd = String(date.getDate()).padStart(2, "0");
+  const mm = String(date.getMonth() + 1).padStart(2, "0");
+  const yyyy = date.getFullYear();
+  return `${yyyy}-${mm}-${dd}`;
+};
+
+const MesButton = ({ n }: { n: number }) => {
+  const { setInicio, setFinal } = useData();
+
+  function setMes(n: number) {
+    const date = new Date();
+    date.setMonth(date.getMonth() + n);
+
+    const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+    const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+    setInicio(formatDate(firstDay));
+    setFinal(formatDate(lastDay));
+  }
+
+  return (
+    <button style={style} onClick={() => setMes(n)}>
+      {nomeMes(n)}
+    </button>
+  );
+};
+
+export default MesButton;
--- src/Pages/Resumo/index.tsx
+++ src/Pages/Resumo/index.tsx
@@ -1,10 +1,9 @@
-import React from "react";
 import { useData } from "../../Context/DataContext";
 
 const Resumo = () => {
   const { data } = useData();
 
-  console.log(data)
+  console.log(data);
 
   return <div>Resumo</div>;
 };
