12d212d2f7a8cf5abcc3ad98b7194716a1437650
Anderson Santana <ander0308@yahoo.com.br>
2023-08-24 00:32:16 -0300

    feat: add resumo

--- src/Pages/Resumo/index.tsx
+++ src/Pages/Resumo/index.tsx
@@ -3,9 +3,43 @@ import { useData } from "../../Context/DataContext";
 const Resumo = () => {
   const { data } = useData();
 
-  console.log(data);
+  if (data === null) return null;
+  return (
+    <section>
+      <div className="resumo mb flex">
+        <div className="box">
+          <h2>Vendas</h2>
+          <span>
+            {data
+              .filter((item) => item.status !== "falha")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
 
-  return <div>Resumo</div>;
+        <div className="box">
+          <h2>Recebido</h2>
+          <span>
+            {data
+              .filter((item) => item.status === "pago")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+
+        <div className="box">
+          <h2>Processando</h2>
+          <span>
+            {data
+              .filter((item) => item.status === "processando")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+      </div>
+      <div className="box">Gráficos</div>
+    </section>
+  );
 };
 
 export default Resumo;
