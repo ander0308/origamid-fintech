fbe94a1aece2ab0a8e494631aeaf37e72da0f840
Anderson Santana <ander0308@yahoo.com.br>
2023-08-27 23:34:23 -0300

    feat: add component venda and loading

---
+++ src/Components/Loading/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const style: React.CSSProperties = {
+  border: "var(--gap-small) solid var(--color-3)",
+  borderRightColor: "var(--color-4)",
+  width: "var(--gap-default)",
+  height: "var(--gap-default)",
+  borderRadius: "50%",
+  animation: "spin 1s infinite"
+}
+const Loading = () => {
+  return (
+    <div style={style}></div>
+  )
+}
+
+export default Loading
--- src/Pages/Venda/index.tsx
+++ src/Pages/Venda/index.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { useParams } from "react-router-dom";
+import useFetch from "../../Hooks/useFetch";
+import React from "react";
+import { IVenda } from "../../Context/DataContext";
+import Loading from "../../Components/Loading";
 
+type IVendaSemData = Omit<IVenda, "data">;
 const Venda = () => {
-  return (
-    <div>Venda</div>
-  )
-}
+  const { id } = useParams();
+  const { data, loading } = useFetch<IVendaSemData>(
+    `https://data.origamid.dev/vendas/${id}`
+  );
 
-export default Venda
+  if (loading === true) return <Loading />;
+  if (data === null) return null;
+
+  return (
+    <div>
+      <div className="box mb">ID: {data.id}</div>
+      <div className="box mb">Nome: {data.nome}</div>
+      <div className="box mb">
+        Preço:{" "}
+        {data.preco.toLocaleString("pt-br", {
+          style: "currency",
+          currency: "BRL",
+        })}
+      </div>
+      <div className="box mb">Status: {data.status}</div>
+      <div className="box mb">Pagamento: {data.pagamento}</div>
+    </div>
+  );
+};
+
+export default Venda;
